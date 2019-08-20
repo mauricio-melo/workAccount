ea318c3533dca942928fc96ed954506dd62d4523
Maurício Ribeiro de Melo - MML <mauricio.melo@iteris.local>
2019-08-20 11:47:41 -0300

    Ajuste cors

--- src/main/java/com/workAccount/config/CorsConfig.java
+++ src/main/java/com/workAccount/config/CorsConfig.java
@@ -2,12 +2,10 @@ package com.workAccount.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@EnableWebMvc
-public class WebConfig extends WebMvcConfigurerAdapter {
+public class CorsConfig implements WebMvcConfigurer {
 
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
